853f6522a464666c82ce5f348e52f483aa8674d7
Marcos Vinicius <viniciusfreitasrj17@gmail.com>
2023-01-16 23:55:08 -0300

    refactor: separate controller

--- src/external/web/express/routes/products.routes.ts
+++ src/external/web/express/routes/products.routes.ts
@@ -1,27 +1,16 @@
-import { Router, Response, Request } from 'express'
+import { Router } from 'express'
 import { CreateProductController } from '../../../../infra/controllers/create-product.controller'
 import ProductRepository from '../../../database/product.repository'
 import CreateProductUseCase from '../../../../app/usecases/create-product/create-product.usecase'
 
 export const productRouter = Router()
 
-productRouter.post('/', async (req: Request, res: Response) => {
-  const createProductController = new CreateProductController(
-    new CreateProductUseCase(
-      new ProductRepository()
-    ) 
+const createProductController = new CreateProductController(
+  new CreateProductUseCase(
+    new ProductRepository()
   )
-  
-  try {
-    const input = {
-      name: req.body.name,
-      cost: req.body.cost,
-    }
-    
-    const output = await createProductController.execute(input)
+)
 
-    res.status(200).send(output)
-  } catch (error) {
-    res.status(500).send(error)
-  }
-})
+productRouter.post('/', async (req, res) =>  
+  createProductController.execute(req, res)
+)
--- src/infra/controllers/create-product.controller.ts
+++ src/infra/controllers/create-product.controller.ts
@@ -1,12 +1,23 @@
 import CreateProductUseCase from "../../app/usecases/create-product/create-product.usecase"
-import { CreateProductInputDto } from "../../app/usecases/create-product/create-product.dto"
+import { RequestProtocol, ResponseProtocol } from "./controller.protocol"
 
 export class CreateProductController {
   constructor(
     private _createProductUseCase: CreateProductUseCase,
   ) {}
 
-  public async execute(input: CreateProductInputDto) {
-    return this._createProductUseCase.execute(input)
+  public async execute(req: RequestProtocol, res: ResponseProtocol) {
+    try {
+      const input = {
+        name: req.body.name,
+        cost: req.body.cost,
+      }
+
+      const output = await this._createProductUseCase.execute(input)
+
+      res.status(200).send(output)
+    } catch (error) {
+      res.status(500).send(error)
+    }
   }
 }
